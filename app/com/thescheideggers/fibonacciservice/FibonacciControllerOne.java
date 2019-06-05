185fa8fc03905271f6d4703d0e888fb9f6a24c99
wdschei <Bill.Scheidegger@Rackspace.com>
2019-06-05 14:05:27 -0500

    Added testing files.

---
+++ app/com/thescheideggers/fibonacciservice/FibonacciControllerOne.java
@@ -0,0 +1,41 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Fibonacci Service
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2014 - 2017 Coffeehouse Consultants
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package com.thescheideggers.fibonacciservice;
+
+import play.http.HttpEntity;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class FibonacciControllerOne extends Controller {
+
+    public Result getFibonacciString(String length) {
+        return new Result(OK, HttpEntity.fromString(
+                "Howdy",
+                "utf-8")
+                .as("text/plain"));
+    }
+
+    public Result getFibonacciArray(String length) {
+        return new Result(OK, HttpEntity.fromString(
+                "Howdy",
+                "utf-8")
+                .as("text/plain"));
+    }
+}
